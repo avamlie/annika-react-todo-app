7573e3b0271b3aaa3af0675899b012195002841e
Annika Amlie <aamlie.01@gmail.com>
2024-05-27 18:21:56 -0400

    add styles and modify App to include components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import React, { Component } from 'react';
+import Form from './components/Form'
+import Header from "./components/Header";
+import TODOHero from "./components/TODOHero";
+import TODOList from "./components/TODOList";
+//import crypto from 'crypto';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Home() {
+    //todos holds current value of state, setTodos is used to update the state
+    const [todos, setTodos] = React.useState([]);
+    //get the todos that are already completed
+    const todos_completed = todos.filter(
+      (todo) => todo.is_completed === true
+    ).length;
+    const total_todos = todos.length;
+    return(
+      <div className="wrapper">
+        <Header />
+        <TODOHero todos_completed={todos_completed} total_todos={total_todos} />
+        <Form setTodos={setTodos}/>
+        <TODOList todos={todos} setTodos={setTodos} />
+      </div>
+    );
 }
-
-export default App;
+export default Home;
